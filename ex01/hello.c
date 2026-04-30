94093749be3eec51cbb6b6376dc9a34a87da2e73
ysy123428 <1613594963@qq.com>
2026-04-30 17:00:40 +0800

    版本2：增加C语言键盘输入功能，更新README说明

--- ex01/hello.c
+++ ex01/hello.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
+    // 版本1：基础HelloWorld
     printf("Hello, World!\n");
+
+    // 版本2：增加键盘输入功能
+    char user_input[100];
+    printf("请输入你想说的内容：");
+    fgets(user_input, sizeof(user_input), stdin);
+    // 去掉输入自带的换行符
+    user_input[strcspn(user_input, "\n")] = '\0';
+    printf("你输入的内容是：%s\n", user_input);
+
     return 0;
 }
